13b9e59b22e6486c832b5b726e62873345e345e0
Boss Eggs <wesley.j.locklair@gmail.com>
2024-08-01 22:18:08 -0400

    changed port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5432;
 
 const sess = { //figure out what this does    // used for cookies
   cookie : {
