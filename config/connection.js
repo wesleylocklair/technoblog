5d9118a1a752cfc33354b0abe637742066018296
Boss Eggs <wesley.j.locklair@gmail.com>
2024-08-26 14:33:11 -0400

    server work

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
 require('dotenv').config();
 
 let sequelize;
+
 if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
+  sequelize = new Sequelize (process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
